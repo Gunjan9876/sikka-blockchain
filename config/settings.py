1aea0069b9748e67d5cf4f627ec464a993507467
Gunjan Goyal <gunjangoyal2006@gmail.com>
2026-07-02 12:52:32 +0530

    Phase 0: Critical bug fixes

--- config/settings.py
+++ config/settings.py
@@ -139,6 +139,4 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = "accounts.User"
